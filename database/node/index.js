48e4425e90e7084b44732b399051793f017a9ac2
brenomachadodomonte <brenomachadodomonte@gmail.com>
2022-04-22 12:14:51 -0300

    Creating docker compose for node and mysql

---
+++ database/node/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+const config = {
+    host: 'mysql-db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+const sql = `insert into people(name) values ('Breno')`;
+connection.query(sql);
+connection.end();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Full cycle</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
